269bd4d5738beb842d2afefbb6af4b3d4145ee8e
EDNAN ROGERIO FRIZZERA FILHO <ednan@frizzera.dev>
2023-11-07 13:46:37 -0300

    Video 308

--- Calculator/Controllers/ViewController.swift
+++ Calculator/Controllers/ViewController.swift
@@ -13,10 +13,31 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet var displayLabel: UILabel!
 
-    var isFinishedTypingNumber: Bool = true
+    private var isFinishedTypingNumber: Bool = true
+    private var displayValue: Double {
+        get {
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Cannot convert display label text to a Double!")
+            }
+            return number
+        }
+        set {
+            displayLabel.text = String(newValue)
+        }
+    }
 
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         isFinishedTypingNumber = true
+
+        if let calcMethod = sender.currentTitle {
+            if calcMethod == "+/-" {
+                displayValue *= -1
+            } else if calcMethod == "AC" {
+                displayLabel.text = "0"
+            } else if calcMethod == "%" {
+                displayValue *= 0.01
+            }
+        }
     }
 
     @IBAction func numButtonPressed(_ sender: UIButton) {
@@ -25,6 +46,12 @@ class ViewController: UIViewController {
                 displayLabel.text = numValue
                 isFinishedTypingNumber = false
             } else {
+                if numValue == "." {
+                    let isInt = floor(displayValue) == displayValue
+                    if !isInt {
+                        return
+                    }
+                }
                 displayLabel.text = displayLabel.text! + numValue
             }
         }

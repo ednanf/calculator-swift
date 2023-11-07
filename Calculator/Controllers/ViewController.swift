89c3eec3ddbe82e62ecf464a0f5875ff19eb8e0a
EDNAN ROGERIO FRIZZERA FILHO <ednan@frizzera.dev>
2023-11-07 10:29:07 -0300

    First commit

---
+++ Calculator/Controllers/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Angela Yu on 10/09/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+//  Modified by Ednan R. Frizzera Filho on 06/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var displayLabel: UILabel!
+    
+    var isFinishedTypingNumber: Bool = true
+
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+    }
+
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+    }
+}
